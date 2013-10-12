cd3ea4e81d89dad0d51fe402b1fdfdcb248f4f62
fkorotkov <fkorotkov@twitter.com>
2013-10-11 17:57:44 -0700

    file type and language

---
+++ src/com/intellij/plugins/thrift/ThriftFileType.java
@@ -0,0 +1,48 @@
+package com.intellij.plugins.thrift;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Created by fkorotkov.
+ */
+public class ThriftFileType extends LanguageFileType {
+    public static final ThriftFileType INSTANCE = new ThriftFileType();
+
+    @NonNls
+    public static final String DEFAULT_EXTENSION = "thrift";
+
+    protected ThriftFileType() {
+        super(ThriftLanguage.INSTANCE);
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return ThriftBundle.message("thrift.name");
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return ThriftBundle.message("thrift.description");
+    }
+
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return DEFAULT_EXTENSION;
+    }
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return null;
+    }
+}
---
+++ src/com/intellij/plugins/thrift/ThriftLanguage.java
@@ -0,0 +1,14 @@
+package com.intellij.plugins.thrift;
+
+import com.intellij.lang.Language;
+
+/**
+ * Created by fkorotkov.
+ */
+public class ThriftLanguage extends Language {
+    public static ThriftLanguage INSTANCE = new ThriftLanguage();
+
+    protected ThriftLanguage() {
+        super(ThriftBundle.message("thrift.language.id"));
+    }
+}
