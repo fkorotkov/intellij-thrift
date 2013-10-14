440127a13149d83158a7575677a66030b63afdbe
fkorotkov <fkorotkov@twitter.com>
2013-10-13 18:41:06 -0700

    quote handler

---
+++ src/com/intellij/plugins/thrift/ThriftQuoteHandler.java
@@ -0,0 +1,16 @@
+package com.intellij.plugins.thrift;
+
+import com.intellij.codeInsight.editorActions.QuoteHandler;
+import com.intellij.codeInsight.editorActions.SimpleTokenSetQuoteHandler;
+import com.intellij.plugins.thrift.lang.lexer.ThriftTokenTypeSets;
+import com.intellij.psi.TokenType;
+import com.intellij.util.ArrayUtil;
+
+/**
+ * Created by fkorotkov.
+ */
+public class ThriftQuoteHandler extends SimpleTokenSetQuoteHandler {
+    public ThriftQuoteHandler() {
+        super(ArrayUtil.append(ThriftTokenTypeSets.STRINGS.getTypes(), TokenType.BAD_CHARACTER));
+    }
+}
