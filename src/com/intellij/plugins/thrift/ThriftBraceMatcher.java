d10802b4e41adc42f42f4e4ecac6a08fc3f0fe0a
fkorotkov <fkorotkov@twitter.com>
2013-10-13 18:35:24 -0700

    brace matcher

---
+++ src/com/intellij/plugins/thrift/ThriftBraceMatcher.java
@@ -0,0 +1,36 @@
+package com.intellij.plugins.thrift;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.plugins.thrift.lang.lexer.ThriftTokenTypes;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Created by fkorotkov.
+ */
+public class ThriftBraceMatcher implements PairedBraceMatcher {
+
+    private static final BracePair[] PAIRS = new BracePair[]{
+            new BracePair(ThriftTokenTypes.LEFTCURLYBRACE, ThriftTokenTypes.RIGHTCURLYBRACE, false),
+            new BracePair(ThriftTokenTypes.LEFTBRACE, ThriftTokenTypes.RIGHTBRACE, false),
+            new BracePair(ThriftTokenTypes.LEFTBRACKET, ThriftTokenTypes.RIGHTBRACKET, false)
+    };
+
+    @Override
+    public BracePair[] getPairs() {
+        return PAIRS;
+    }
+
+    @Override
+    public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType lbraceType, @Nullable IElementType contextType) {
+        return true;
+    }
+
+    @Override
+    public int getCodeConstructStart(PsiFile file, int openingBraceOffset) {
+        return openingBraceOffset;
+    }
+}
