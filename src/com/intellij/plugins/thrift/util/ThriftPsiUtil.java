2c741871c144a257f370a819a7bad80917f98984
jcdavis <jackson@jcdav.is>
2015-02-24 10:56:36 -0800

    Fix matching wrong imports

--- src/com/intellij/plugins/thrift/util/ThriftPsiUtil.java
+++ src/com/intellij/plugins/thrift/util/ThriftPsiUtil.java
@@ -24,6 +24,7 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -134,7 +135,7 @@ public class ThriftPsiUtil {
       @Override
       public boolean process(ThriftInclude include) {
         String path = include.getPath();
-        if (FileUtil.getNameWithoutExtension(path).endsWith(fileName)) {
+        if (FileUtil.getNameWithoutExtension(new File(path)).equals(fileName)) {
           result.set(include);
           return false;
         }
