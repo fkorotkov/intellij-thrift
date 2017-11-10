f4cc9c3de6e10e49f260fca1951f63a6c8e9d095
yuokada <callistoiv+git@gmail.com>
2017-11-11 01:08:15 +0900

    Add i8 keyword

--- src/com/intellij/plugins/thrift/util/ThriftUtils.java
+++ src/com/intellij/plugins/thrift/util/ThriftUtils.java
@@ -14,7 +14,7 @@ public class ThriftUtils {
     "xsd_namespace", "cpp", "java", "py", "py.twisted", "perl", "rb", "js", "st",
     "cocoa", "csharp", "c_glib", "go", "php", "delphi", "const", "enum", "exception",
     "senum", "service", "struct", "typedef", "union", "required", "optional", "xsd_optional",
-    "xsd_nillable", "oneway", "throws", "bool", "byte", "i16", "i32", "i64", "double",
+    "xsd_nillable", "oneway", "throws", "bool", "byte", "i8", "i16", "i32", "i64", "double",
     "string", "binary", "slist", "list", "map", "set", "cpp_type", "void", "true", "false"
   );
 
