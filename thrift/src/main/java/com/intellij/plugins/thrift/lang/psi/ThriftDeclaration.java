88d03be4c0639c70c8541c71fe64ac2e45892a85
Tomasz Pasternak <tpasternak@virtuslab.com>
2020-08-27 17:03:52 +0200

    Add context action: Go to Thrift source
    Add an action to context menu that allows user to jump to Thrift definition
    from any place where it is referenced

--- thrift/src/main/java/com/intellij/plugins/thrift/lang/psi/ThriftDeclaration.java
+++ thrift/src/main/java/com/intellij/plugins/thrift/lang/psi/ThriftDeclaration.java
@@ -1,11 +1,12 @@
 package com.intellij.plugins.thrift.lang.psi;
 
 import com.intellij.navigation.NavigationItem;
+import com.intellij.psi.NavigatablePsiElement;
 import com.intellij.psi.PsiNamedElement;
 
 /**
  * Created by fkorotkov.
  */
-public interface ThriftDeclaration extends ThriftPsiCompositeElement, PsiNamedElement, NavigationItem {
+public interface ThriftDeclaration extends ThriftPsiCompositeElement, PsiNamedElement, NavigationItem, NavigatablePsiElement {
   ThriftDefinitionName getIdentifier();
 }
